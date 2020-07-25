59227000d29f7e49ac009b587ede82918ac57936
Romeu Vieira <romeu.bizz@gmail.com>
2020-07-25 18:59:46 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate hyper;
+
+use hyper::Client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // This is where we will setup our HTTP client requests.
+
+    Ok(())
+}
