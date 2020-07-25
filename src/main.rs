0cd63e0353dc78c049c06c403a0cc11a14a19643
Romeu Vieira <romeu.bizz@gmail.com>
2020-07-25 23:00:41 +0100

    Use reqwest // function to execute request and return status code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-extern crate hyper;
+use reqwest;
 
-use hyper::Client;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // This is where we will setup our HTTP client requests.
+fn request(url: &str) -> Result<u16> {
+	let resp = reqwest::blocking::get(url)?
+		.status()
+		.as_u16();
 
-    Ok(())
+	return Ok(resp);
+}
+
+fn main() -> Result<()> {
+	let result = request("https://google.com/")?;
+	println!("{}", result);
+	// match resp {
+	// 	Ok(v) => println!("working with version: {:?}", v),
+	// 	Err(e) => println!("error parsing header: {:?}", e),
+	// }
+
+    return Ok(());
 }
