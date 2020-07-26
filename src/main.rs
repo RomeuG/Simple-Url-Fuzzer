f8ee5540ce9f926333215764ffc7e0b338f521e8
Romeu Vieira <romeu.bizz@gmail.com>
2020-07-26 01:44:07 +0100

    Get cmd args // concatenate strings and request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use reqwest;
 
+use std::env;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn request(url: &str) -> Result<u16> {
+	println!("Initiating request to {}...", url);
+
 	let resp = reqwest::blocking::get(url)?
 		.status()
 		.as_u16();
@@ -28,17 +31,30 @@ fn load_file(file: &str) -> Result<Vec<String>> {
 }
 
 fn main() -> Result<()> {
-	let result = request("https://google.com/")?;
-	println!("{}", result);
-	// match resp {
-	// 	Ok(v) => println!("working with version: {:?}", v),
-	// 	Err(e) => println!("error parsing header: {:?}", e),
-	// }
 
-	let file_lines = load_file("wordlist.txt")?;
-	for line in file_lines {
-		println!("{}", line);
+	// args
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() < 3 {
+		println!("Not enough arguments!");
+		std::process::exit(1);
 	}
+
+	let url = &args[1];
+	let file = &args[2];
+
+	let result = request(&url)?;
+	println!("{}", result);
+
+	let file_lines = load_file(&file)?;
+
+	for line in file_lines {
+		let new_url = url.to_owned() + &line;
+		let result = request(&new_url)?;
+		println!("{}", result);
+	}
+
+	println!("Finishing...");
 
     return Ok(());
 }
