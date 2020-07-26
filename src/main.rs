4d65d49498a813e2dc9e799530a0f1c68ed247d9
Romeu Vieira <romeu.bizz@gmail.com>
2020-07-26 01:07:33 +0100

    Load file lines into vector

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use reqwest;
 
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn request(url: &str) -> Result<u16> {
@@ -10,6 +13,20 @@ fn request(url: &str) -> Result<u16> {
 	return Ok(resp);
 }
 
+fn load_file(file: &str) -> Result<Vec<String>> {
+	let mut vector = Vec::new();
+
+	let file = File::open(file)?;
+	let reader = BufReader::new(file);
+
+	for line in reader.lines() {
+        //println!("{}", line?);
+		vector.push(line?);
+    }
+
+	return Ok(vector);
+}
+
 fn main() -> Result<()> {
 	let result = request("https://google.com/")?;
 	println!("{}", result);
@@ -17,6 +34,11 @@ fn main() -> Result<()> {
 	// 	Ok(v) => println!("working with version: {:?}", v),
 	// 	Err(e) => println!("error parsing header: {:?}", e),
 	// }
+
+	let file_lines = load_file("wordlist.txt")?;
+	for line in file_lines {
+		println!("{}", line);
+	}
 
     return Ok(());
 }
