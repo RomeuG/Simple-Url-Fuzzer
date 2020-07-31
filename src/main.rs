e5091db156abdd2fcc07cd771bbb98a626115488
Romeu Vieira <romeu.bizz@gmail.com>
2020-07-31 22:10:02 +0100

    General formatting

--- src/main.rs
+++ src/main.rs
@@ -21,26 +21,26 @@ type HttpResult<T, K> = std::result::Result<T, K>;
 
 #[derive(Default)]
 struct Statistics {
-	codes: HashMap<String, Vec<String>>,
-	errors: HashMap<String, Vec<String>>,
+    codes: HashMap<String, Vec<String>>,
+    errors: HashMap<String, Vec<String>>,
 }
 
 fn error_handler(error: &reqwest::Error) -> String {
-	if (error.is_body()) {
+    if (error.is_body()) {
 		return "BODY".to_string();
-	} else if (error.is_decode()) {
+    } else if (error.is_decode()) {
 		return "DECODE".to_string();
-	} else if (error.is_status()) {
+    } else if (error.is_status()) {
 		return "STATUS".to_string();
-	} else if (error.is_builder()) {
+    } else if (error.is_builder()) {
 		return "BUILDER".to_string();
-	} else if (error.is_timeout()) {
+    } else if (error.is_timeout()) {
 		return "TIMEOUT".to_string();
-	} else if (error.is_redirect()) {
+    } else if (error.is_redirect()) {
 		return "REDIRECT".to_string();
-	} else {
+    } else {
 		return "UNKNOWN".to_string();
-	}
+    }
 }
 
 fn request(url: &str) -> HttpResult<u16, String> {
@@ -59,7 +59,7 @@ fn request(url: &str) -> HttpResult<u16, String> {
 }
 
 fn worker(thread_id: u32, url: String, lines: Arc<Mutex<Vec<String>>>, stats: Arc<Mutex<Statistics>>) {
-	loop {
+    loop {
 		let mut line_mutex = lines.lock().unwrap();
 
 		if (line_mutex.len() < 1) {
@@ -95,7 +95,7 @@ fn worker(thread_id: u32, url: String, lines: Arc<Mutex<Vec<String>>>, stats: Ar
 				}
 			}
 		}
-	}
+    }
 }
 
 fn load_file(file: &str) -> Result<Vec<String>> {
@@ -112,33 +112,33 @@ fn load_file(file: &str) -> Result<Vec<String>> {
 }
 
 fn main() -> Result<()> {
-	let matches = App::new("url-fuzzer")
+    let matches = App::new("url-fuzzer")
     	.version("0.1")
     	.author("Romeu Gomes <romeu.bizz@gmail.com>")
     	.about("This is an URL Fuzzer.")
     	.arg(Arg::with_name("url")
-        	 .short("u")
-        	 .long("url")
-        	 .value_name("URL")
-        	 .help("Url to fuzz")
-        	 .takes_value(true))
+             .short("u")
+             .long("url")
+             .value_name("URL")
+             .help("Url to fuzz")
+             .takes_value(true))
     	.arg(Arg::with_name("wordlist")
 			 .short("w")
-        	 .long("wordlist")
-        	 .value_name("FILE")
-        	 .help("Wordlist with 1 word per line")
-        	 .takes_value(true))
+             .long("wordlist")
+             .value_name("FILE")
+             .help("Wordlist with 1 word per line")
+             .takes_value(true))
     	.arg(Arg::with_name("threads")
-        	 .short("t")
-        	 .long("threads")
-        	 .value_name("N")
-        	 .help("Number of threads")
-        	 .takes_value(true))
+             .short("t")
+             .long("threads")
+             .value_name("N")
+             .help("Number of threads")
+             .takes_value(true))
     	.get_matches();
 
-	let url = matches.value_of("url").unwrap().to_owned();
-	let file = matches.value_of("wordlist").unwrap();
-	let nthreads = matches.value_of("threads").unwrap();
+    let url = matches.value_of("url").unwrap().to_owned();
+    let file = matches.value_of("wordlist").unwrap();
+    let nthreads = matches.value_of("threads").unwrap();
 
     if !url.contains("@@") {
 		println!("Fuzzing indicator not present!");
@@ -147,34 +147,34 @@ fn main() -> Result<()> {
 
     let file_lines = Arc::new(Mutex::new(load_file(&file)?));
 
-	// threading stuff
-	let mut threads = Vec::new();
-	let mut _stats = Statistics {
+    // threading stuff
+    let mut threads = Vec::new();
+    let mut _stats = Statistics {
 		codes: HashMap::new(),
 		errors: HashMap::new(),
-	};
+    };
 
-	let mut stats: Arc<Mutex<Statistics>> = Arc::new(Mutex::new(_stats));
+    let mut stats: Arc<Mutex<Statistics>> = Arc::new(Mutex::new(_stats));
 
-	for thread_id in 0..nthreads.parse::<u32>().unwrap() {
+    for thread_id in 0..nthreads.parse::<u32>().unwrap() {
 		let t_url = url.clone();
 		let vec_clone = file_lines.clone();
 		let stats_clone = stats.clone();
 
 		threads.push(std::thread::spawn(move || worker(thread_id, t_url.to_string(), vec_clone, stats_clone)));
-	}
+    }
 
-	for thr in threads {
+    for thr in threads {
 		thr.join().unwrap();
-	}
+    }
 
-	let mut stats_mutex = stats.lock().unwrap();
+    let mut stats_mutex = stats.lock().unwrap();
 
-	for (key, value) in stats_mutex.errors.clone() {
+    for (key, value) in stats_mutex.errors.clone() {
 		for item in value {
 			println!("{} / {}", key, item);
 		}
-	}
+    }
 
     println!("Finishing...");
 
